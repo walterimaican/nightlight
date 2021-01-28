4030dd913a26403d72f8ba1c46033f50c3ddc333
walterimaican <walwillwess@gmail.com>
2021-01-28 13:25:47 -0600

    Initialize new console application and gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nightlight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
