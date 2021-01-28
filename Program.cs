57dbe80bfa383aacbd1b9e96dc14065c723b3291
walterimaican <walwillwess@gmail.com>
2021-01-28 14:32:21 -0600

    basic system tray icon

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+// using System.Collections.Generic;
+// using System.Configuration;
+// using System.Data;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using System.Windows;
+using System.Windows.Forms;
+using System.Drawing;
 
-namespace nightlight
+namespace Nightlight
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Application.EnableVisualStyles();
+            // Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new NightlightApplication());
+        }
+    }
+
+    class NightlightApplication : ApplicationContext
+    {
+        private NotifyIcon nightlight;
+
+        public NightlightApplication()
+        {
+            nightlight = new NotifyIcon();
+            nightlight.Icon = new Icon("assets/lightswitch.ico");
+            nightlight.ContextMenuStrip = new ContextMenuStrip();
+            nightlight.ContextMenuStrip.Items.Add("Exit", Image.FromFile("assets/lightswitch.ico"), myExit);
+            nightlight.Visible = true;
+        }
+
+        void myExit(object sender, EventArgs e)
+        {
+            nightlight.Visible = false;
+            Application.Exit();
         }
     }
 }
