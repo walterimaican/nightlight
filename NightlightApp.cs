cbd15d56e88199098155a98c28155b2e3b974d2e
walterimaican <walwillwess@gmail.com>
2021-01-30 12:11:22 -0600

    added settings with apps and system toggles

--- NightlightApp.cs
+++ NightlightApp.cs
@@ -16,6 +16,8 @@ namespace Nightlight
         /* Constants */
         private const String LIGHT_ICON_PATH = "assets/light.ico";
         private const String DARK_ICON_PATH = "assets/dark.ico";
+        private const String SETTINGS_ICON_PATH = "assets/settings.ico";
+        private const String HELP_ICON_PATH = "assets/help.ico";
         private const String ICON_LIGHT_TEXT = "Nightlight: Light";
         private const String ICON_DARK_TEXT = "Nightlight: Dark";
         private Color LIGHT_MODE_BACKGROUND = Color.WhiteSmoke;
@@ -42,17 +44,42 @@ namespace Nightlight
             darkModeButton.Text = "Activate Dark Mode";
             darkModeButton.Click += OnDarkMode;
 
+            // Misnomer: The affected registry is "AppsUseLightTheme",
+            // however in effect, this is what flips the whole system.
+            // By default, this option is enabled.
+            ToolStripMenuItem appsThemeButton = new ToolStripMenuItem();
+            appsThemeButton.Text = "Apps, Explorer, System";
+            appsThemeButton.CheckOnClick = true;
+            appsThemeButton.Click += OnAppsToggle;
+            appsThemeButton.Checked = true;
+
+            // Although the affected registry is "SystemUsesLightTheme",
+            // this only flips Start Menu and Taskbar
+            // By default, this option is NOT enabled.
+            ToolStripMenuItem systemThemeButton = new ToolStripMenuItem();
+            systemThemeButton.Text = "Start Menu && Taskbar";
+            systemThemeButton.CheckOnClick = true;
+            systemThemeButton.Click += OnSystemToggle;
+
+            ToolStripMenuItem settingsButton = new ToolStripMenuItem();
+            settingsButton.Image = Image.FromFile(SETTINGS_ICON_PATH);
+            settingsButton.Text = "Settings";
+            settingsButton.DropDownItems.Add(appsThemeButton);
+            settingsButton.DropDownItems.Add(systemThemeButton);
+
             ToolStripMenuItem aboutButton = new ToolStripMenuItem();
-            aboutButton.Text = "About";
-            aboutButton.Click += OnAbout;
+            aboutButton.Image = Image.FromFile(HELP_ICON_PATH);
+            aboutButton.Text = "Help && About";
+            aboutButton.Click += OnHelpAndAbout;
 
             ToolStripMenuItem exitButton = new ToolStripMenuItem();
             exitButton.Text = "Exit";
             exitButton.Click += OnExit;
 
-            ToolStripItem[] toolStripItems = new ToolStripItem[] {
+            ToolStripMenuItem[] toolStripMenuItems = new ToolStripMenuItem[] {
                 lightModeButton,
                 darkModeButton,
+                settingsButton,
                 aboutButton,
                 exitButton
             };
@@ -60,7 +87,7 @@ namespace Nightlight
             // Context Menu
             _contextMenuStrip = new ContextMenuStrip();
             _contextMenuStrip.ShowItemToolTips = false;
-            _contextMenuStrip.Items.AddRange(toolStripItems);
+            _contextMenuStrip.Items.AddRange(toolStripMenuItems);
             _contextMenuStrip.Renderer = new CustomRenderer();
 
             // Notify Icon
@@ -82,10 +109,19 @@ namespace Nightlight
 
         private void activateLightMode()
         {
-            foreach (ToolStripItem item in _contextMenuStrip.Items)
+            foreach (ToolStripMenuItem item in _contextMenuStrip.Items)
             {
                 item.BackColor = LIGHT_MODE_BACKGROUND;
                 item.ForeColor = LIGHT_MODE_TEXT;
+
+                if (item.HasDropDownItems)
+                {
+                    foreach (ToolStripMenuItem subItem in item.DropDownItems)
+                    {
+                        subItem.BackColor = LIGHT_MODE_BACKGROUND;
+                        subItem.ForeColor = LIGHT_MODE_TEXT;
+                    }
+                }
             }
             _notifyIcon.Icon = _lightIcon;
             _notifyIcon.Text = ICON_LIGHT_TEXT;
@@ -95,10 +131,19 @@ namespace Nightlight
 
         private void activateDarkMode()
         {
-            foreach (ToolStripItem item in _contextMenuStrip.Items)
+            foreach (ToolStripMenuItem item in _contextMenuStrip.Items)
             {
                 item.BackColor = DARK_MODE_BACKGROUND;
                 item.ForeColor = DARK_MODE_TEXT;
+
+                if (item.HasDropDownItems)
+                {
+                    foreach (ToolStripMenuItem subItem in item.DropDownItems)
+                    {
+                        subItem.BackColor = DARK_MODE_BACKGROUND;
+                        subItem.ForeColor = DARK_MODE_TEXT;
+                    }
+                }
             }
             _notifyIcon.Icon = _darkIcon;
             _notifyIcon.Text = ICON_DARK_TEXT;
@@ -135,7 +180,20 @@ namespace Nightlight
             activateDarkMode();
         }
 
-        void OnAbout(object sender, EventArgs e)
+        void OnAppsToggle(object sender, EventArgs e)
+        {
+            ToolStripMenuItem appsButton = sender as ToolStripMenuItem;
+            _themeSwitcher.setShouldToggleApps(appsButton.Checked);
+        }
+
+        void OnSystemToggle(object sender, EventArgs e)
+        {
+            ToolStripMenuItem systemButton = sender as ToolStripMenuItem;
+            _themeSwitcher.setShouldToggleSystem(systemButton.Checked);
+
+        }
+
+        void OnHelpAndAbout(object sender, EventArgs e)
         {
             String url = "https://github.com/walterimaican/nightlight";
 
--- ThemeSwitcher.cs
+++ ThemeSwitcher.cs
@@ -5,19 +5,22 @@ namespace Nightlight
 {
     class ThemeSwitcher
     {
-        // TODO
-        // Offer SystemUsesLightTheme as well (flips taskbar and startmenu)
-        // AppsUseLightTheme is for everything else
         private bool _isLight;
+        private bool _shouldToggleApps;
+        private bool _shouldToggleSystem;
 
         /* Constants */
         private const String REG_KEY = "HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize";
-        private const String REG_VALUE = "AppsUseLightTheme";
+        private const String REG_VALUE_APPS = "AppsUseLightTheme";
+        private const String REG_VALUE_SYSTEM = "SystemUsesLightTheme";
 
         public ThemeSwitcher()
         {
-            // Get current value - default to dark mode if not found 
-            _isLight = Convert.ToBoolean((Int32)Registry.GetValue(REG_KEY, REG_VALUE, 0));
+            // Get current value from apps registry - default to dark mode if not found 
+            this._isLight = Convert.ToBoolean((Int32)Registry.GetValue(REG_KEY, REG_VALUE_APPS, 0));
+
+            // By default, enable toggling of apps, disable toggling of system
+            this._shouldToggleApps = true;
         }
 
         public bool getIsLight()
@@ -25,15 +28,43 @@ namespace Nightlight
             return this._isLight;
         }
 
+        public void setShouldToggleApps(bool value)
+        {
+            this._shouldToggleApps = value;
+        }
+
+        public void setShouldToggleSystem(bool value)
+        {
+            this._shouldToggleSystem = value;
+        }
+
         public void setThemeToLight()
         {
-            Registry.SetValue(REG_KEY, REG_VALUE, 1, RegistryValueKind.DWord);
+            if (this._shouldToggleApps)
+            {
+                Registry.SetValue(REG_KEY, REG_VALUE_APPS, 1, RegistryValueKind.DWord);
+            }
+
+            if (this._shouldToggleSystem)
+            {
+                Registry.SetValue(REG_KEY, REG_VALUE_SYSTEM, 1, RegistryValueKind.DWord);
+            }
+
             this._isLight = true;
         }
 
         public void setThemeToDark()
         {
-            Registry.SetValue(REG_KEY, REG_VALUE, 0, RegistryValueKind.DWord);
+            if (this._shouldToggleApps)
+            {
+                Registry.SetValue(REG_KEY, REG_VALUE_APPS, 0, RegistryValueKind.DWord);
+            }
+
+            if (this._shouldToggleSystem)
+            {
+                Registry.SetValue(REG_KEY, REG_VALUE_SYSTEM, 0, RegistryValueKind.DWord);
+            }
+
             this._isLight = false;
         }
     }
